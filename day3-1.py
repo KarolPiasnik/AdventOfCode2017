81c5ec32ed670adf80a457fc5a613f4a7fb63ca9
KarolPiasnik <piasnik.karol@outlook.com>
2017-12-03 20:54:42 +0100

    solved with magic numbers :)

---
+++ day3-1.py
@@ -0,0 +1,9 @@
+import math
+print(math.sqrt(265149)) #as it is square matrix this will give approximation of its size
+print(515**2)
+print(514**2)
+print(265149-514**2)#number of elements on outer layer
+print(515//2+1)#number of layers from middle
+print(953-514)
+print(439-258) # distance sideways
+print(257+181) 
